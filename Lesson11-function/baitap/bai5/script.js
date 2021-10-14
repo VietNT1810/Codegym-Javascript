0bbc082ec6b97d00c6fe21d02e695e61fd1944ea
Nguyễn Tuấn Việt <tuanviet19xx@gmail.com>
2021-10-14 11:39:17 +0700

    bai 5 lesson 11

--- Lesson11-function/baitap/bai5/script.js
+++ Lesson11-function/baitap/bai5/script.js
@@ -8,13 +8,18 @@ function addBtn() {
     count++;
     document.getElementById('countProduct').innerHTML = `${count} product`;
 
+
     let row = table.insertRow(-1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
     cell1.innerHTML = `${count}`;
-    cell2.innerHTML = newProduct.value;
-    cell3.innerHTML = '<button onclick="deleteRow(this)">Delete</button>';
+    cell2.innerHTML = `<div id="editAble">${newProduct.value}</div>`;
+    cell3.innerHTML = '<button onclick="editRow(this)">Edit</button>';
+    cell4.innerHTML = '<button onclick="saveRow(this)">Save</button>';
+    cell5.innerHTML = '<button onclick="deleteRow(this)">Delete</button>';
 
     newProduct.value = '';
 }
@@ -23,5 +28,20 @@ function addBtn() {
 function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("productTable").deleteRow(i);
-    count--;
+    for (var i = 1; i < productTable.rows.length; i++) {
+        productTable.rows[i].cells[0].innerHTML = i;
+        document.getElementById('countProduct').innerHTML = `${i} product`;
+    }   
+}
+
+//Edit row
+function editRow(r) {
+    let selectedRow = r.parentElement.parentElement;
+    selectedRow.cells[1].setAttribute('contenteditable','true');
+}
+
+//Save row
+function saveRow(r) {
+    let selectedRow = r.parentElement.parentElement;
+    selectedRow.cells[1].removeAttribute('contenteditable','true');
 }
