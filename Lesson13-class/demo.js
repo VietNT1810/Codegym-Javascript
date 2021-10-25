61a61d864950ed7b6c6f09bdb7aeafcabd3b511f
Nguyễn Tuấn Việt <tuanviet19xx@gmail.com>
2021-10-25 16:18:49 +0700

    Add btvn lesson13
    Signed-off-by: Nguyễn Tuấn Việt <tuanviet19xx@gmail.com>

---
+++ Lesson13-class/demo.js
@@ -0,0 +1,20 @@
+
+// class Students {
+//     constructor(id, name, address) {
+//         this.theId = id;
+//         this.theName = name;
+//         this.theAddress = address;
+//     }
+
+//     getInfo() {
+//         return `id = ${this.theId}, name = ${this.theName}, address = ${this.theAddress}`;
+//     }
+// }
+
+
+// var student = new Students(1, 'viet', 'hanoi');
+// console.log(student.getInfo());
+
+var d = new Date();
+var day = d.getDate();
+console.log(day);
